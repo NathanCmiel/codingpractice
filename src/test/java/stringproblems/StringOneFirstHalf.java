cd21ea094f2239627133cfb05d3e358df866d54b
Nathan Cmiel <cetan25@gmail.com>
2021-05-17 23:41:40 -0400

    Implemented StringOneWithoutEnd.java class.

--- src/test/java/stringproblems/StringOneFirstHalf.java
+++ src/test/java/stringproblems/StringOneFirstHalf.java
@@ -1,2 +1,14 @@
-package stringproblems;public class StringOneFirstHalf {
+package stringproblems;
+
+public class StringOneFirstHalf {
+    /*************************************************************************
+     * Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+     *
+     * firstHalf("WooHoo") → "Woo"
+     * firstHalf("HelloThere") → "Hello"
+     * firstHalf("abcdef") → "abc"
+     ************************************************************************/
+    public String firstHalf(String str) {
+        return str.substring(0,str.length()/2);
+    }
 }
